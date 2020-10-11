04617d3b4a675dfa2640bfa227347ce410674daa
Den Mor <kievden26092014@gmail.com>
2020-10-11 14:52:20 +0300

    5 changes

--- calculator.cpp
+++ calculator.cpp
@@ -1,15 +1,18 @@
 #include "calculator.h"
 
+//function Add 
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
 }
 
+//function Sub
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
 
+//function Mult
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
--- calculator.h
+++ calculator.h
@@ -1,6 +1,8 @@
 #ifndef CALCULATOR_H
 #define CALCULATOR_H
 
+//class Calculator
+//
 class Calculator
 {
     public:
